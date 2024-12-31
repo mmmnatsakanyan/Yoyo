5cc1224409b1c0c2ed088ed3d4b1372e60a63e69
mmmnatsakanyan <mnatsakanyan.arm10@gmail.com>
2024-12-31 14:48:15 +0400

    Create script.py

---
+++ script.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+import yt_dlp
+import requests
+
+app = Flask(__name__)
+
+YOAI_API_KEY = 'ваш_api_ключ'
+YOAI_SEND_MESSAGE_URL = 'https://yoai.yophone.com/api/pub/sendMessage'
+
+def download_audio(url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': 'audio.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    return 'audio.mp3'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    message = data.get('message', '')
+    chat_id = data.get('chat', {}).get('id', '')
+
+    if 'youtube.com' in message or 'youtu.be' in message:
+        mp3_file = download_audio(message)
+        files = {'file': open(mp3_file, 'rb')}
+        headers = {'X-YoAI-API-Key': YOAI_API_KEY}
+        payload = {'chat_id': chat_id, 'caption': 'Ваш MP3 файл'}
+        response = requests.post(YOAI_SEND_MESSAGE_URL, headers=headers, data=payload, files=files)
+        return jsonify(response.json())
+    return 'OK'
+
+if __name__ == '__main__':
+    app.run(port=5000)
